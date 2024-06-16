c771dce48362c6ddf6bba3f23cfd5a1a65d4ef6d
Hari Om <hari333333om@gmail.com>
2024-06-17 00:27:37 +0530

    Initial Commit

---
+++ src/WeatherApp.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+import SearchBox from "./SearchBox";
+import InfoBox from "./InfoBox";
+
+import "./WeatherApp.css";
+
+export default function WeatherApp() {
+  let [weatherInfo, setWeatherInfo] = useState();
+
+  let updateInfo = (newInfo) => {
+    setWeatherInfo(newInfo);
+  };
+
+  return (
+    <div>
+      <h1 style={{ textAlign: "center", color: "#1976d2" }}>Weather App</h1>
+      <SearchBox updateInfo={updateInfo} />
+      {weatherInfo && <InfoBox info={weatherInfo} />}
+      <div className="footer">
+        Git Repo:-{" "}
+        <a href="https://github.com/HarryOhm33/Weather-App.git">Click Here!</a>{" "}
+        &nbsp;|&nbsp; LinkedIn:- <a href="">Click Here!</a>
+      </div>
+    </div>
+  );
+}
